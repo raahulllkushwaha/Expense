5a43d7c34c035c6d13fbffafea42deef0bd7f66f
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-06-21 11:39:05 +0530

    Initial commit

--- src/main/java/com/rahul/expense/ExpenseApplication.java
+++ src/main/java/com/rahul/expense/ExpenseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpenseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseApplication.class, args);
+    }
 
 }
