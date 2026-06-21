b0170e3a6a66e252cd91ff4358751d37bd36b127
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-06-21 12:08:20 +0530

    Add repository

---
+++ src/main/java/com/rahul/expense/repo/TenantRepo.java
@@ -0,0 +1,9 @@
+package com.rahul.expense.repo;
+
+import com.rahul.expense.entity.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TenantRepo extends JpaRepository<Tenant, Long> {
+}
